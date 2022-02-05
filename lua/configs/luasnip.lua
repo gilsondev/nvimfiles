15d6199c5fc4ed0b4a2cdb9d3848efa3759a5036
Gilson Filho <me@gilsondev.in>
2022-02-05 19:58:11 -0300

    Remove config filetypes to python modules in django project

--- lua/configs/luasnip.lua
+++ lua/configs/luasnip.lua
@@ -3,20 +3,9 @@ local M = {}
 function M.config()
   require("luasnip/loaders/from_vscode").lazy_load()
 
-  -- Snippets to Django
-  vim.cmd [[au BufRead,BufNewFile */admin.py set filetype=python.django]]
-  vim.cmd [[au BufRead,BufNewFile */models.py set filetype=python.django]]
-  vim.cmd [[au BufRead,BufNewFile */views.py set filetype=python.django]]
-  vim.cmd [[au BufRead,BufNewFile */forms.py set filetype=python.django]]
-  vim.cmd [[au BufRead,BufNewFile */urls.py set filetype=python.django]]
-  vim.cmd [[au BufRead,BufNewFile */tests.py set filetype=python.django]]
-  vim.cmd [[au BufRead,BufNewFile */tests/*.py set filetype=python.django]]
-  vim.cmd [[au BufRead,BufNewFile */manage.py set filetype=python.django]]
+  -- Snippets to Django Templates
   vim.cmd [[au BufRead,BufNewFile */templates/*.html set filetype=htmldjango]]
   vim.cmd [[au BufRead,BufNewFile */templates/**/*.html set filetype=htmldjango]]
-
-  -- Snippets to Django REST Framework
-  vim.cmd [[au BufRead,BufNewFile */serializers.py set filetype=python.drf]]
 end
 
 return M
